7518f263b28f5a3b64f316d01adad687ae590a7c
Mouhiba <127572447+Mouhiba@users.noreply.github.com>
2023-04-04 13:30:23 +0100

    Update Ch-26-Checkpoint_iris.py

--- Ch-26-Checkpoint_iris.py
+++ Ch-26-Checkpoint_iris.py
@@ -1,4 +1,11 @@
+
 import streamlit as st
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
 st.title  ("CC Hallo")
 
 
